4481b305aee1d66e85f9a0af8210b9cd88360561
DERRADJI <amine.derradji@capgemini.com>
2018-07-06 11:10:28 +0200

    fix color button PP

--- dist/js/seche.js
+++ dist/js/seche.js
@@ -7,8 +7,12 @@
       $(".ppcomp2").removeAttr('id');
       $(".ppcomp1").css("display","none");
       $(".ppcomp2").css("display","none");
+
       $("#tagbtn1").addClass("active-btn-blue");
+      $("#tagbtn1").removeClass("btnGray");
       $("#tagbtn2").removeClass("active-btn-blue");
+      $("#tagbtn2").addClass("btnGray");
+
       $("#firstComp").css("display","block");
 
       $("#devis-gratuit").removeAttr('href');
@@ -22,8 +26,12 @@
       $(".ppcomp2").attr('id', 'thirdComp');
       $(".ppcomp1").css("display","none");
       $(".ppcomp2").css("display","none");
+
       $("#tagbtn2").addClass("active-btn-blue");
+      $("#tagbtn2").removeClass("btnGray");
       $("#tagbtn1").removeClass("active-btn-blue");
+      $("#tagbtn1").addClass("btnGray");
+
       $("#firstComp").css("display","block");
 
       $("#devis-gratuit").removeAttr('href');
